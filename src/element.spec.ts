316ce93ead870ad9911be92cb26854f009efb6e0
Vadim Shashin <enapiuz@gmail.com>
2021-08-08 02:48:05 +0300

    basic test for Element

---
+++ src/element.spec.ts
@@ -0,0 +1,30 @@
+import {Element} from "./element";
+import {BUF} from "./gates/buf";
+import {Dataset} from "./types";
+
+describe("Element", () => {
+    class BufElem extends Element {
+        formBoard() {
+            this.addElement("buf", new BUF());
+            this.addInput(0, "buf", 0);
+            this.addOutput(0, "buf", 0);
+        }
+    }
+    const dataset: Dataset[] = [
+        {
+            input: [[0, false]],
+            expect: [[0, false]],
+        },
+        {
+            input: [[0, true]],
+            expect: [[0, true]],
+        },
+    ];
+    dataset.forEach((test) => {
+        it("works with just BUF", () => {
+            const elem = new BufElem();
+            const res = elem.eval(new Map(test.input));
+            expect(res).toEqual(new Map(test.expect));
+        });
+    });
+});
